b7be3ea43638cd8622323762e0538ad0be560798
Mikhail8425 <mikhailshemet@gmail.com>
2024-03-04 22:24:45 -0500

    exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,9 +9,6 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
-Store model (class): `has_many :employees`
-Employee model (class): `belongs_to :store`
-
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 @store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 60)
 @store1.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 60)
@@ -19,17 +16,17 @@ Employee model (class): `belongs_to :store`
 @store1.employees.create(first_name: "Jane", last_name: "Smith", hourly_rate: 60)
 @store1.employees.create(first_name: "John", last_name: "Johnson", hourly_rate: 60)
 @store1.employees.create(first_name: "Jane", last_name: "Johnson", hourly_rate: 60)
-@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 60)
-@store1.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 60)
-@store2.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 60)
-@store2.employees.create(first_name: "Jane", last_name: "Smith", hourly_rate: 60)
-@store2.employees.create(first_name: "John", last_name: "Johnson", hourly_rate: 60)
-@store2.employees.create(first_name: "Jane", last_name: "Johnson", hourly_rate: 60)
-@store2.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 60)
-@store2.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 60)
-@store2.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 60)
-@store2.employees.create(first_name: "Jane", last_name: "Smith", hourly_rate: 60)
-@store2.employees.create(first_name: "John", last_name: "Johnson", hourly_rate: 60)
-@store2.employees.create(first_name: "Jane", last_name: "Johnson", hourly_rate: 60)
-@store2.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 60)
-@store2.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Does", hourly_rate: 60)
+@store1.employees.create(first_name: "Jane", last_name: "Does", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Smithons", hourly_rate: 60)
+@store2.employees.create(first_name: "Jane", last_name: "Smithons", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Johnsonsons", hourly_rate: 60)
+@store2.employees.create(first_name: "Jane", last_name: "Johnsonsons", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Doemills", hourly_rate: 60)
+@store2.employees.create(first_name: "Jane", last_name: "Doemills", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Smithlangs", hourly_rate: 60)
+@store2.employees.create(first_name: "Jane", last_name: "Smithlangs", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Johnsones", hourly_rate: 60)
+@store2.employees.create(first_name: "Jane", last_name: "Johnsones", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Doepongopolos", hourly_rate: 60)
+@store2.employees.create(first_name: "Jane", last_name: "Doepongopolos", hourly_rate: 60)
