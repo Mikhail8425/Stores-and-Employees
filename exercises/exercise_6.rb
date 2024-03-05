014cf1b27039af409f788107e2a2346c58c876d8
Mikhail8425 <mikhailshemet@gmail.com>
2024-03-04 22:07:10 -0500

    exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,27 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+Store model (class): `has_many :employees`
+Employee model (class): `belongs_to :store`
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 60)
+@store1.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 60)
+@store1.employees.create(first_name: "Jane", last_name: "Smith", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Johnson", hourly_rate: 60)
+@store1.employees.create(first_name: "Jane", last_name: "Johnson", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 60)
+@store1.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 60)
+@store2.employees.create(first_name: "Jane", last_name: "Smith", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Johnson", hourly_rate: 60)
+@store2.employees.create(first_name: "Jane", last_name: "Johnson", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 60)
+@store2.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 60)
+@store2.employees.create(first_name: "Jane", last_name: "Smith", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Johnson", hourly_rate: 60)
+@store2.employees.create(first_name: "Jane", last_name: "Johnson", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 60)
+@store2.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 60)
